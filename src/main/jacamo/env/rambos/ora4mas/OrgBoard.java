48896076d1cb895c531904984bd8cfc4fc871595
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-06-27 02:43:56 -0300

    Remove unused attribute from OrgBoard
    Signed-off-by: Igor C. A. de Lima <igorcadelima@gmail.com>

--- src/main/jacamo/env/rambos/ora4mas/OrgBoard.java
+++ src/main/jacamo/env/rambos/ora4mas/OrgBoard.java
@@ -41,7 +41,6 @@ import rambos.ora4mas.util.DJUtil;
 import rambos.os.OS;
 
 public class OrgBoard extends ora4mas.nopl.OrgBoard {
-  protected String osFile = null;
   protected ArtifactId deJure;
   protected OS os;
 
@@ -55,8 +54,6 @@ public class OrgBoard extends ora4mas.nopl.OrgBoard {
    * @param osFile path to organisation specification
    */
   public void init(final String osFile) {
-    this.osFile = osFile;
-
     try {
       Document doc = DJUtil.parseDocument(osFile);
       Node nsNode = doc.getElementsByTagName(NS.getXMLTag())
