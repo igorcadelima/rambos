cec1ad05a1fb79fbb884ac595d5099e7e916c3ab
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-07-05 11:52:23 -0300

    Create interface for sanction builder

---
+++ src/main/jacamo/env/rambos/ISanctionBuilder.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) Igor Conrado Alves de Lima <igorcadelima@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package rambos;
+
+import jason.asSyntax.LogicalFormula;
+
+/**
+ * @author igorcadelima
+ *
+ */
+public interface ISanctionBuilder<T extends ISanctionBuilder<T>> extends Builder<ISanction> {
+  /**
+   * Set id for the sanction.
+   * 
+   * @param id sanction's id
+   * @return builder builder instance
+   */
+  T setId(String id);
+
+  /**
+   * Set state for the sanction.
+   * 
+   * @param state sanction's state
+   * @return builder builder instance
+   */
+  T setState(State state);
+
+  /**
+   * Set condition for the sanction.
+   * 
+   * @param condition sanction's activation condition
+   * @return builder builder instance
+   */
+  T setCondition(LogicalFormula condition);
+
+  /**
+   * Set category for the sanction.
+   * 
+   * @param category sanction category
+   * @return builder builder instance
+   */
+  T setCategory(SanctionCategory category);
+
+  /**
+   * Set content for the norm.
+   * 
+   * @param content norm's content
+   * @return builder builder instance
+   */
+  T setContent(IContent content);
+
+  /**
+   * Build sanction based on the data provided using the setter methods.
+   * 
+   * @return new sanction
+   */
+  ISanction build();
+}
